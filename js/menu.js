19aa5a2e374b9bd273d0ded3c0c287bf06bbf85a
GustavoGmBr <gustavonovaisgm@gmail.com>
2025-06-21 11:58:08 -0300

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('menu.html')
+    .then(response => {
+      if (!response.ok) throw new Error("Menu não encontrado!");
+      return response.text();
+    })
+    .then(html => {
+      document.getElementById('menu-sidebar').innerHTML = html;
+    })
+    .catch(error => {
+      console.error("Erro ao carregar menu:", error);
+    });
+});
